24181687cc9ca91ce6f88325032d6d4bffb3afe0
samuelsharaf <samuel.sharaf@salesforce.com>
2017-07-22 14:50:16 -0700

    added setup.sh

---
+++ setup.sh
@@ -0,0 +1,27 @@
+echo "\n-----------------------------"
+echo "Updating Node..."
+echo "-----------------------------\n"
+
+. ~/.profile
+nvm install 7
+nvm alias default 7
+
+echo "\n-----------------------------"
+echo "Updating Heroku CLI..."
+echo "-----------------------------\n"
+
+heroku update
+
+echo "\n-----------------------------"
+echo "Setting up Artillary..."
+echo "-----------------------------\n"
+
+npm install -g artillery
+
+echo "\n-----------------------------"
+
+artillery dino
+
+echo "\n-----------------------------"
+echo "All Set!..."
+echo "-----------------------------\n"
