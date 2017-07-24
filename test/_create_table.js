3fa7f18d9f1a30885c825585eca185ad877438ce
Samuel Sharaf <sam@heroku.com>
2017-07-24 14:36:20 -0700

    Update _create_table.js

--- test/_create_table.js
+++ test/_create_table.js
@@ -1,21 +1,10 @@
 require('env2')('.env'); // see: https://github.com/dwyl/env2
 var assert = require('assert');
-var pg = require('pg');
 
-function create_tables (callback) {
-  var client = new pg.Client(process.env.DATABASE_URL);
-  client.connect(function(err) {
-    assert(!err); // die if we cannot connect
-    var file = require('path').resolve('./lib/database_setup.sql');
-    var query = require('fs').readFileSync(file, 'utf8').toString();
-    console.log('\n', query);
-    client.query(query, function(err, result) {
-      client.end(); // close connection to database
-      return callback(err, result);
-    });
-  });
-}
-
-create_tables(function (err, data) {
-  console.log(err, data, 'DB Table Created & Test Data Inserted');
-});
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query(
+  'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
