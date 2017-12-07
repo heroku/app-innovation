41e0a9c714054085bc696126c49b1ca2ca204451
bigpugin97 <34350855+bigpugin97@users.noreply.github.com>
2017-12-07 13:17:24 -0800

    Update index.js

--- index.js
+++ index.js
@@ -74,3 +74,16 @@ app.get('/children', function (req, res, next) {
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
+
