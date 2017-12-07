38597e25c85033ce072fc45486e8e6979b8dfbd7
puchaj <jaya.syamala.p@gmail.com>
2017-12-07 13:29:46 -0800

    Update index.js

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ app.listen(app.get('port'), function() {
 
 app.get('/db', function (request, response) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
-    client.query('SELECT * FROM test_table', function(err, result) {
+    client.query('SELECT * FROM Account', function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
