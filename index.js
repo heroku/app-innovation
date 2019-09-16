a2bc90e4e3b8d5449227191a90dda06364a927a6
mburnside <mburnside@salesforce.com>
2019-09-17 09:43:25 +1000

    addedd database table and js function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var pg = require('pg');
+
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -33,6 +35,18 @@ app.get('/atan2/:count', (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(x));
 })
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) { 
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('pages/db', {results: result.rows} ); }
+    });
+  });
+});
 
 app.get('/rockets', function (req, res, next) {
   throw new Error('Kaboom!');
